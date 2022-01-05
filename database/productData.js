b503fc2391a4a41bff82af2971d57f8596fb5597
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-05 11:37:17 -0700

    Add a starting dataset

---
+++ database/productData.js
@@ -0,0 +1,94 @@
+let productData = [
+  {
+    name: 'Sculpt Tank',
+    color: 'pink',
+    img_url:'https://images.lululemon.com/is/image/lululemon/LW1CS9S_049106_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category:'tops'
+  },
+  {
+    name: 'All Yours Tank',
+    color: 'white',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW1CGLS_0002_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category:'tops'
+  },
+  {
+    name: 'Full Day Ahead Short Sleeve Shirt',
+    color: 'white',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW3EMPS_049466_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'tops'
+  },
+  {
+    name: 'All Days Shirt',
+    color: 'white',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW3DZYS_0002_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'tops'  
+  },
+  {
+    name: 'Cut Back Crop Tank Top',
+    color: 'white',
+    img_url: 'https://shop.lululemon.com/p/women-tanks/Cut-Back-Crop-Tank-MD/_/prod8670155?color=37121',
+    category: 'tops'
+  },
+  {
+    name: 'lululemon Align™ Jogger Cool *Vent',
+    color: 'purple',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW5CT1S_045720_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'bottoms'
+  },
+  {
+    name: 'Fast and Free High Rise Tight',
+    color: 'brown',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW5CTRS_046553_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'bottoms'
+  },
+  {
+    name: 'Wunder Under High-Rise Tight',
+    color: 'black',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW5BP4S_0001_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'bottoms'
+  },
+  {
+    name: 'Invigorate High-Rise Tight',
+    color: 'brown',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW5CX1S_045881_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'bottoms'
+  },
+  {
+    name: 'Wunder Train High-Rise Tight',
+    color: 'blue',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW5CQDS_027590_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'bottoms'
+  },
+  {
+    name: 'Cross Paths Rucksack',
+    color: 'green',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW9BOJS_044629_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'accessories'
+  },
+  {
+    name: 'Light Locks Scrunchie',
+    color: 'white',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW9CDCS_044981_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'accessories'
+  },
+  {
+    name: 'All Hours Belt Bag',
+    color: 'black',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW9CJZS_028928_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'accessories'
+  },
+  {
+    name: 'Fill your Day Backpack',
+    color: 'brown',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW9CQGS_034093_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'accessories'
+  },
+  {
+    name: 'City Adventurer Backpack',
+    color: 'red',
+    img_url: 'https://images.lululemon.com/is/image/lululemon/LW9DMTS_051630_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
+    category: 'accessories'
+  }
+]
+
+module.exports = productData;

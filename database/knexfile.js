0e9ae5b593c96293dcadb1bc9cab141e73cba053
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-05 12:00:15 -0700

    Reconfigure knex file to indicate where our seed data is located

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,11 +1,12 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/lemon_aide',
     migrations: {
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }

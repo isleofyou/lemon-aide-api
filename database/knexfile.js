2ecd551fef1c11e3c92ddffd76e274c1644aed3f
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-05 10:23:46 -0700

    Configure the knex file to indicate where we're going to store our migrations

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,11 +1,12 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/products',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
